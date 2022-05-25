afab2ccf8643a7a72de9e08174357b908de4d228
Perc <38033198+Percronos@users.noreply.github.com>
2022-05-25 13:53:10 +0200

    Update eth.go

--- app/ante/eth.go
+++ app/ante/eth.go
@@ -121,9 +121,9 @@ func (avd EthAccountVerificationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx
 				"the sender is not EOA: address %s, codeHash <%s>", fromAddr, acct.CodeHash)
 		}
 
-		if err := evmkeeper.CheckSenderBalance(sdk.NewIntFromBigInt(acct.Balance), txData); err != nil {
-			return ctx, sdkerrors.Wrap(err, "failed to check sender balance")
-		}
+		//if err := evmkeeper.CheckSenderBalance(sdk.NewIntFromBigInt(acct.Balance), txData); err != nil {
+		//	return ctx, sdkerrors.Wrap(err, "failed to check sender balance")
+		//}
 
 	}
 	return next(ctx, tx, simulate)
